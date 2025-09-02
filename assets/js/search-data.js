8f5fb80832b0c5adb717d2cc0db98dd5e567df82
ngol0 <44044782+ngol0@users.noreply.github.com>
2025-09-02 15:03:15 +0000

    Deploying to gh-pages from @ ngol0/ngol0.github.io@28b9535abfc83e59fb2da45a2c8783dfaddfbcbe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "All publications",
+          description: "all publications",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
