a608ec2021f177851ac17aab86e4e6d2d49a80ad
ngol0 <44044782+ngol0@users.noreply.github.com>
2025-09-23 15:04:14 +0000

    Deploying to gh-pages from @ ngol0/ngol0.github.io@a40f0a1c0e7ce4ba0ee47bcdc98df8d78dc5033e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,9 +440,9 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-computer-vision-projects",
+          title: 'Computer Vision Projects',
+          description: "self-directed learning projects",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
