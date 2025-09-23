c308bd5892668c51d77b10c408ce5e5102a5b504
ngol0 <44044782+ngol0@users.noreply.github.com>
2025-09-23 15:58:31 +0000

    Deploying to gh-pages from @ ngol0/ngol0.github.io@71950f2455751ba1fd1b767929f5419974469516 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,6 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-computer-vision-projects",
-          title: 'Computer Vision projects',
-          description: "self learning projects",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
@@ -455,6 +450,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-computer-vision-projects",
+          title: 'Computer Vision projects',
+          description: "self learning projects",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/computer_vision/";
             },},{
         id: 'social-email',
         title: 'email',
